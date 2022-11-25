a77dd277a6703e8d2ef98ec345dc663eeaffd782
Karol Banat <karol.banat@gmail.com>
2022-11-25 16:12:52 +0100

    Simple game menu error message

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,10 @@
 const gameMenu: HTMLElement = document.querySelector('#game-menu');
+/* form elements */
 const gameMenuForm: HTMLFormElement = gameMenu.querySelector('.game-menu-form');
 const menuFormOptions: NodeListOf<HTMLInputElement> = gameMenuForm.querySelectorAll('input[type="radio"]');
+const menuErrorMsg: HTMLElement = gameMenuForm.querySelector('[role="alert"]');
 const menuFormSubmitButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll('button[type="submit"]');
+/* game view: container with header, game board and scores */
 const ticTacToeGameView: HTMLElement = document.querySelector('#tic-tac-toe-game');
 
 const isAnyOptionChecked = () => {
@@ -17,7 +20,15 @@ const getCheckedOptionMark = (): string => {
 
 const handleMenuSubmitButton = (ev: Event) => {
 	ev.preventDefault();
-	if (!isAnyOptionChecked()) return;
+	/* Checks if any mark is selected. If none then displays error */
+	if (!isAnyOptionChecked()) {
+		menuErrorMsg.classList.add('visible');
+		menuErrorMsg.innerText = 'Please select mark for player 1';
+		return;
+	} else {
+		menuErrorMsg.classList.remove('visible');
+		menuErrorMsg.innerText = '';
+	}
 
 	// const submitBtn: HTMLButtonElement = ev.target as HTMLButtonElement;
 	// const gameMode: string = submitBtn.dataset.mode;
